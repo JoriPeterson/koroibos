0ef24fa34016fb4754697165b5a95bafaf793deb
Jori Peterson <joripeterson@gmail.com>
2019-11-03 13:23:43 -0700

    Add validations to model spec and model.rb

---
+++ spec/models/olympian_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Olympian, type: :model do
+  describe 'Validations' do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :sex}
+    it {should validate_presence_of :age}
+    it {should validate_presence_of :height}
+    it {should validate_presence_of :weight}
+    it {should validate_presence_of :team}
+    it {should validate_presence_of :games}
+    it {should validate_presence_of :sport}
+    it {should validate_presence_of :event}
+  end
+end
