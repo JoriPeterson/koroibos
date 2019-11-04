8d64fffe91b104467c1b8fc4495c211c8c7c0a2f
Jori Peterson <joripeterson@gmail.com>
2019-11-04 16:07:19 -0700

    Add to olympian request spec for stats

--- spec/requests/api/v1/olympian_request_spec.rb
+++ spec/requests/api/v1/olympian_request_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe "Olympians API" do
   before :each do
-    @athlete_1 = create(:athlete, name: "John", team: "USA", age: 25, sport: "basketball", total_medals_won: 0)
-    @athlete_2 = create(:athlete, name: "Sarah", team: "USA", age: 25, sport: "volleyball", total_medals_won: 1)
-    @athlete_3 = create(:athlete, name: "George", team: "France", age: 21, sport: "running", total_medals_won: 2)
-    @athlete_4 = create(:athlete, name: "Pierre", team: "France", age: 28, sport: "curling", total_medals_won: 3)
+    @athlete_1 = create(:athlete, name: "John", team: "USA", age: 25, sport: "basketball", total_medals_won: 0, sex: "M", weight: 70)
+    @athlete_2 = create(:athlete, name: "Sarah", team: "USA", age: 25, sport: "volleyball", total_medals_won: 1, sex: "F", weight: 65)
+    @athlete_3 = create(:athlete, name: "George", team: "France", age: 21, sport: "running", total_medals_won: 2, sex: "M", weight: 80)
+    @athlete_4 = create(:athlete, name: "Pierre", team: "France", age: 28, sport: "curling", total_medals_won: 3, sex: "M", weight: 75)
   end
 
   it "sends a list of all olympians" do
@@ -40,4 +40,19 @@ describe "Olympians API" do
     expect(oldest['attributes']['name']).to eq("Pierre")
     expect(oldest['attributes']['age']).to eq(28)
   end
+
+  it "gets olympian stats" do
+
+    get '/api/v1/olympian_stats'
+
+    stats = JSON.parse(response.body)['data']
+
+    expect(response).to be_successful
+
+    expect(stats['attributes']['olympian_stats']['total_competing_olympians']).to eq(4)
+    # expect(stats['attributes']['olympian_stats']['female_olympians']).to eq(65.0)
+    # expect(stats['attributes']['olympian_stats']['male_olympians']).to eq(75.0)
+    expect(stats['attributes']['olympian_stats'].values.last).to eq(24.8)
+  end
+  
 end
