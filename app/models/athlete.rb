7943ddce6b4d2f94985b5c4379a97f9ed0aba8b4
Jori Peterson <joripeterson@gmail.com>
2019-11-03 20:56:00 -0700

    Add methods for youngest and oldest olympians

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -4,4 +4,12 @@ class Athlete < ApplicationRecord
   validates_presence_of :age
   validates_presence_of :sport
   validates_presence_of :total_medals_won
+
+  def self.youngest_olympian
+    order(age: :asc).first
+  end
+
+  def self.oldest_olympian
+    order(age: :desc).first
+  end
 end
