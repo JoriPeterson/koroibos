eef8474c842be83fbdaedf0e24c3bde139be5645
Jori Peterson <joripeterson@gmail.com>
2019-11-03 19:59:19 -0700

    Update olympian factory

--- spec/factories/olympians.rb
+++ spec/factories/olympians.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :olympian do
-    name { "MyString" }
-    sex { "MyString" }
-    age { 25 }
-    height { 165 }
-    weight { 65 }
-    team { "MyString" }
-    games { "MyString" }
-    sport { "MyString" }
-    event { "MyString" }
-    medal { "MyString" }
+    name { "Charter" }
+		sex { "M" }
+		age { 25 }
+		height { 160 }
+		weight { 70 }
+		team { "USA" }
+		games { "2016 Summer" }
+		sport { "Bowling" }
+		event { "Bowling 2000" }
+		medal { "Bronze" }
   end
 end
