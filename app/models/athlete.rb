ba9a5a02168af3cfb17a0179afa2f22e70d9b39d
Jori Peterson <joripeterson@gmail.com>
2019-11-04 16:08:53 -0700

    Update athletes model

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -3,6 +3,8 @@ class Athlete < ApplicationRecord
   validates_presence_of :team
   validates_presence_of :age
   validates_presence_of :sport
+  validates_presence_of :weight
+  validates_presence_of :sex
   validates_presence_of :total_medals_won
 
   def self.youngest_olympian
@@ -11,5 +13,17 @@ class Athlete < ApplicationRecord
 
   def self.oldest_olympian
     order(age: :desc).first
+  end
+
+  def self.total_competing_olympians
+    select(:name).count
+  end
+
+  def self.average_weight(sex)
+    select(:sex).where("sex = ?", sex).average(:weight).round(1).to_f
+  end
+
+  def self.average_age
+    average(:age).round(1).to_f
   end
 end
--- db/migrate/20191104021358_create_athletes.rb
+++ db/migrate/20191104021358_create_athletes.rb
@@ -6,6 +6,8 @@ class CreateAthletes < ActiveRecord::Migration[5.2]
       t.integer :age
       t.string :sport
       t.integer :total_medals_won
+      t.integer :weight
+      t.string :sex
 
       t.timestamps
     end
