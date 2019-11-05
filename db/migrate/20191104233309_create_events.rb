8274253fb4066000b9e2ce694b74f20e67d431f0
Jori Peterson <joripeterson@gmail.com>
2019-11-04 19:34:52 -0700

    Update migrations and rake task for last endpoint

--- db/migrate/20191104233309_create_events.rb
+++ db/migrate/20191104233309_create_events.rb
@@ -2,6 +2,7 @@ class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
       t.string :event
+      t.string :medal
       t.references :sport, foreign_key: true
 
       t.timestamps
