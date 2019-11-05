01c3ba9a905d2e154854ef889c6349a0a1cf5311
Jori Peterson <joripeterson@gmail.com>
2019-11-04 19:35:26 -0700

    Change 'Athlete' model to 'Olympian' model

--- app/controllers/api/v1/olympians_controller.rb
+++ app/controllers/api/v1/olympians_controller.rb
@@ -2,11 +2,11 @@ class Api::V1::OlympiansController < ApplicationController
 
   def index
     if params[:age] == "youngest"
-      render json: AthleteSerializer.new(Athlete.youngest_olympian)
+      render json: OlympianSerializer.new(Olympian.youngest_olympian)
     elsif params[:age] == "oldest"
-      render json: AthleteSerializer.new(Athlete.oldest_olympian)
+      render json: OlympianSerializer.new(Olympian.oldest_olympian)
     else
-      render json: AthleteSerializer.new(Athlete.all)
+      render json: OlympianSerializer.new(Olympian.all)
     end
   end
 end
--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -1,11 +1,30 @@
 class Olympian < ApplicationRecord
   validates_presence_of :name
-  validates_presence_of :sex
-  validates_presence_of :age
-  validates_presence_of :height
-  validates_presence_of :weight
   validates_presence_of :team
-  validates_presence_of :games
+  validates_presence_of :age
   validates_presence_of :sport
-  validates_presence_of :event
+  validates_presence_of :weight
+  validates_presence_of :sex
+  validates_presence_of :total_medals_won
+  validates_presence_of :medal
+
+  def self.youngest_olympian
+    order(age: :asc).first
+  end
+
+  def self.oldest_olympian
+    order(age: :desc).first
+  end
+
+  def self.total_competing_olympians
+    select(:name).count
+  end
+
+  def self.average_weight(sex)
+    select(:sex).where("sex = ?", sex).average(:weight).round(1).to_f
+  end
+
+  def self.average_age
+    average(:age).round(1).to_f
+  end
 end
