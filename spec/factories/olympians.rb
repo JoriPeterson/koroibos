0deacf08f49419beb409cbaffc6572f2312e913b
Jori Peterson <joripeterson@gmail.com>
2019-11-04 19:34:25 -0700

    Update tests for Olympian

--- spec/factories/olympians.rb
+++ spec/factories/olympians.rb
@@ -1,14 +1,12 @@
 FactoryBot.define do
   factory :olympian do
-    name { "Charter" }
-		sex { "M" }
-		age { 25 }
-		height { 160 }
-		weight { 70 }
-		team { "USA" }
-		games { "2016 Summer" }
-		sport { "Bowling" }
-		event { "Bowling 2000" }
-		medal { "Bronze" }
+    name { "MyString" }
+    team { "MyString" }
+    age { 1 }
+    sport { "MyString" }
+    total_medals_won { 1 }
+    weight { 70 }
+    sex { "MyString" }
+    medal { "Bronze" }
   end
 end
